b4b66197d72c28e3ee9454c479e0ac055d0a4eaf
Nikki Victoria Sollid <nikki.sollid@gmail.com>
2023-01-24 21:52:52 +0100

    #4: Added react icons library (#16)

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Color } from "./constant";
+import { FaBeer } from "react-icons/fa";
+import { DiAndroid } from "react-icons/di";
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hola ben, spicy taco {Color["primary"].slot_01}</Text>
       <StatusBar style="auto" />
+      <DiAndroid />
+      <FaBeer />
     </View>
   );
 }
