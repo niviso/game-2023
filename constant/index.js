f945b63fc55e27b5fb60ed51cc944333098fa203
Nikki Victoria Sollid <nikki.sollid@gmail.com>
2023-01-24 21:29:12 +0100

    2 foundation variables REFACTOR (#15)
    * #2: Rename of files
    
    * #2: Solved error

--- constant/index.js
+++ constant/index.js
@@ -1,5 +1,9 @@
-import { View } from "react-native";
+import Color from "./Color";
+import Size from "./Size";
+import Font from "./Font";
+import Image from "./Image";
+import Sound from "./Sound";
+import Style from "./Style";
+import Theme from "./Theme";
 
-export default function Start() {
-  return <View />;
-}
+export { Color, Size, Font, Image, Sound, Style, Theme };
