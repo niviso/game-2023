858557fccc0e426b479b430b7358e9c41a08445c
Nikki Victoria Sollid <nikki.sollid@gmail.com>
2023-05-29 10:25:13 +0200

    #7: added paused state to countdown

--- views/Game/index.tsx
+++ views/Game/index.tsx
@@ -98,12 +98,14 @@ export default function Game({setCurrentPath}:any) {
   },[gameState]);
 
   useInterval(() => {
+    if(!gameState.paused){
     if(countDown === 0){
       setGameTime(Time.getMinutes(1));
       setActive(true);
       setGameState((prevState) => ({...prevState,mode: GameMode.none}));
     }
     setCountDown((prevCountDown) => (prevCountDown - 1));
+    }
   },countDown === -1 ? null : Time.getSecondsInMs(Time.second));
 
   useInterval(() => {
