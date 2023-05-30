18f544af755a23e9b2fcfe494af56ef018c9f499
Nikki Victoria Sollid <nikki.sollid@gmail.com>
2023-05-30 11:41:07 +0200

    #6: Need to refactor button

--- views/Select/index.tsx
+++ views/Select/index.tsx
@@ -52,10 +52,10 @@ export default function Select({ setCurrentPath }) {
         </Animatable.Text>
       </TouchableOpacity>
       <Animatable.View animation="slideInUp" duration={500} onTouchStart={() => setCurrentPath("Start")} style={{position: "absolute",shadowColor: '#171717',shadowOffset: {width: 0, height: 4},shadowOpacity: 0.5,shadowRadius: 5,borderTopRightRadius:10,display: "flex",alignItems:"center",justifyContent:"center",bottom:0,left:0,width: 140,height: 80,backgroundColor: "white",zIndex:99}}>
-      <Animatable.Text style={{fontSize: 20,color: color}}>BACK</Animatable.Text>
+      <Animatable.Text animation="pulse" iterationCount="infinite" duration={1000} delay={1000} style={{fontSize: 20,color: color}}>BACK</Animatable.Text>
     </Animatable.View>
     <Animatable.View animation="slideInUp" duration={500} onTouchStart={() => setCurrentPath("OnBoard")} style={{position: "absolute",shadowColor: '#171717',shadowOffset: {width: 0, height: 4},shadowOpacity: 0.5,shadowRadius: 5,borderTopLeftRadius:10,display: "flex",alignItems:"center",justifyContent:"center",bottom:0,right:0,width: 140,height: 80,backgroundColor: "white",zIndex:99}}>
-    <Animatable.Text style={{fontSize: 20,color: color}}>TUTORIAL</Animatable.Text>
+    <Animatable.Text animation="pulse" iterationCount="infinite" duration={1000} delay={1000} style={{fontSize: 20,color: color}}>TUTORIAL</Animatable.Text>
     </Animatable.View>
     </View>
   );
