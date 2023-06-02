222429762262e2ef3fd1ed2950a5a3dde9da377d
Nikki Victoria Sollid <nikki.sollid@gmail.com>
2023-06-02 11:07:32 +0200

    #28: Translated onboarding

--- views/OnBoard/index.tsx
+++ views/OnBoard/index.tsx
@@ -1,6 +1,7 @@
 import { OnboardFlow } from 'react-native-onboard';
 import {Image,Text} from "react-native";
 import {AsyncStorageHelper} from "../../helpers";
+import i18n from "../../locales";
 
 export default function OnBoard({setRoute}) {
     const onDone = async () => {
@@ -9,41 +10,37 @@ export default function OnBoard({setRoute}) {
         setRoute({path:"Game",data:{}});
 
     }
-    const stepOne = <Text>In this tutorial, we will walk you through the process of onboarding and getting started with a game for the first time. Whether youre a seasoned gamer or new to the gaming scene, this guide will help you navigate the initial stages and ensure a smooth and enjoyable gaming experience.</Text>
-    const stepTwo = <Text style={{textAlign:"center"}}>Look at the colored squares below the upper circle and bar.
-            {"\n\n"}
-        Click or tap on the square that matches the color precisely. Be careful to choose the exact match.
-        The game may have a time limit for each match, so make sure to act swiftly.
-        </Text>;
-    const stepThree:any = <Text style={{textAlign:"center"}}>Upon selecting the correct colored square, you will earn points based on your accuracy and speed.{"\n\n"}This also adds power to your power meter.{"\n"} The player with the highest points wins when the timer ends.</Text>;
-    const stepFour:any = <Text style={{textAlign:"center"}}>When the power meter is filled the stop button will appear.{"\n\n"}It diables the opponent for 3 seconds, if they click on a square during the this time they loose all of their points and power.</Text>;
-
     return (
       <OnboardFlow
         pages={[
           {
-            title: 'Welcome to the exciting world of Colors! ',
-            subtitle: stepOne,
+            primaryButtonTitle: i18n.t("view.onBoard.next"),
+            title: i18n.t("view.onBoard.step_1.title"),
+            subtitle: i18n.t("view.onBoard.step_1.description"),
             imageUri: Image.resolveAssetSource(require('./tutorial_step_1.png')).uri,
           },
           {
-            title: 'How to play the game',
-            subtitle: stepTwo,
+            primaryButtonTitle: i18n.t("view.onBoard.next"),
+            title: i18n.t("view.onBoard.step_2.title"),
+            subtitle: i18n.t("view.onBoard.step_2.description"),
             imageUri: Image.resolveAssetSource(require('./tutorial_step_2.png')).uri,
         },
         {
-          title: 'Gaining points',
-          subtitle: stepThree,
+          primaryButtonTitle: i18n.t("view.onBoard.next"),
+          title: i18n.t("view.onBoard.step_3.title"),
+          subtitle: i18n.t("view.onBoard.step_3.description"),
           imageUri: Image.resolveAssetSource(require('./tutorial_step_3.png')).uri,
         },
         {
-          title: '[STOP]',
-          subtitle: stepFour,
+          primaryButtonTitle: i18n.t("view.onBoard.start"),
+          title: i18n.t("view.onBoard.step_4.title"),
+          subtitle: i18n.t("view.onBoard.step_4.description"),
           imageUri: Image.resolveAssetSource(require('./tutorial_step_4.png')).uri,
         }
         ]}
         type={'fullscreen'}
         onDone={onDone}
+        
       />
     );
   };
